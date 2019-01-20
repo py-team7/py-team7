d0fe04d38ab0a66ebcffc0f0298fbba368d2ab81
shintaro sodeya <shin.1315@outlook.jp>
2019-01-20 15:32:18 +0900

    fin

--- quizans.py
+++ quizans.py
@@ -1,3 +1,27 @@
-from quizapp import Question
+from graphics import *
+from button import Button 
+class Quizans:
+    def __init__(self, win):
+        self.win = win
+        self.aButton = Button(win, Point(56, 55), 25, 10, "A") 
+        self.aButton.activate() 
+        self.bButton = Button(win, Point(144, 55), 25, 10, "B")
+        self.bButton.activate()
+        self.nextButton = Button(win, Point(186.5, 45), 13, 8, "NEXT")
+        self.nextButton.activate()
 
-class Answer:
+    def answer(self):
+        
+        # Event loop 
+        pt = self.win.getMouse() 
+        if self.aButton.clicked(pt): 
+            return "A"
+        if self.bButton.clicked(pt):
+            return "B"
+        pt = self.win.getMouse()
+        
+    def next(self):
+        pt = self.win.getMouse() 
+        if self.nextButton.clicked(pt): 
+             return True
+        pt = self.win.getMouse() 
--- quizapp.py
+++ quizapp.py
@@ -1,6 +1,145 @@
-import quizans from Answer
+from quizans import *
+from graphics import *
+from button import Button
 
 class Question:
+    def __init__(self, win):
+        self.questions = [
+                        {"question":"AXELL", "answer":"A", "comment":"AXELL is an idol group of five people."},
+                        {"question":"asfi", "answer":"A", "comment":"asfi is an idol group in Shibuya."},
+                        {"question":"Ada", "answer":"B", "comment":"Ada is a programming language.\n" + " \n" + "It derives from the name of Ada Lovelace, the world's first programmer."},
+                        {"question":"Curl", "answer":"B", "comment":"Curl is a programming language."},
+                        {"question":"IDL", "answer":"B", "comment":"IDL is a programming language.\n" + " \n" +"IDL stands for Interactive Data Language"},
+                        {"question":"P.IDL", "answer":"A", "comment":"P.IDL is a female idol group"},
+                        {"question":"Cupitron", "answer":"A", "comment":"Cupitron is a trio techno pop idol."},
+                        {"question":"Clojure", "answer":"B", "comment":"Clojure is a programming language."},
+                        {"question":"GEM", "answer":"A", "comment":"GEM is a female idol group"},
+                        {"question":"Ook!", "answer":"B", "comment":"Ook! is a programming language made for orangutans by joke.\n" + " \n" + "Programming can be done just by listening to the orangutan 's bark."}
+                        ]
+        self.win = win
+        self.num = 0
+
+    #消去
+    def undraw(self):
+        self.message_ans.undraw()
+        self.message_right.undraw()
+    
+    #問題数の取得
+    def quizlen(self):
+        length = len(self.questions)
+        return length
+    
+    #問題文の取得
+    def getquiz(self, num):
+        self.num = num
+        self.question = self.questions[self.num]["question"]
+        return self.question
+
+    #解答の取得
+    def getanswer(self):
+        self.answer = self.questions[self.num]["answer"]
+        return self.answer
+    
+    #問題文の表示
+    def lookquiz(self):
+        if self.num != 0:
+            self.message_quiz.undraw()
+        self.message_quiz = Text(Point(40, 140), self.questions[self.num]["question"])
+        self.message_quiz.setSize(24)
+        self.message_quiz.draw(self.win)
+
+    #解説の表示
+    def lookans(self):
+        if self.num!= 0:
+            self.message_ans.undraw()
+        self.message_ans = Text(Point(100, 30), self.questions[self.num]["comment"])
+        self.message_ans.draw(self.win)
+    
+    #合否の表示
+    def lookright(self, ans):
+        if self.num!= 0:
+            self.message_right.undraw()
+        if(self.answer == ans):
+            self.message_right = Text(Point(100, 85), "Yeah! Got it!")
+            self.message_right.setFill("blue")
+        else:
+            self.message_right = Text(Point(100, 85), "Wrong!")
+            self.message_right.setFill("red")
+        self.message_right.setSize(36)
+        self.message_right.draw(self.win)
+
+    def result(self, point):
+        if point >= 8:
+            good = Image(Point(100, 75), "congratulation.png")
+            good.draw(self.win)
+        else:
+            white = bad = Image(Point(100, 75), "white.gif")
+            white.draw(self.win)
+            bad = Image(Point(100, 75), "mission_failed.png")
+            bad.draw(self.win)
+
+        message_point = Text(Point(100, 40), str(point) + " / 10")
+        message_point.setSize(36)
+        
+        message_point.draw(self.win)
+
+
+class Launch:
+    def __init__(self):
+        self.win = GraphWin("A or P Quiz", 800, 600)
+        self.win.setCoords(0, 0, 200, 150)
+        self.win.setBackground("white")
+
+        rect1 = Rectangle(Point(26, 45), Point(86, 130))
+        rect1.setFill("lightgray")
+        rect1.draw(self.win)
+
+        rect2 = Rectangle(Point(114, 45), Point(174, 130))
+        rect2.setFill("lightgray")
+        rect2.draw(self.win)
+
+        pink = Image(Point(56, 100), "pink.png")
+        pink.draw(self.win)
+
+        blue = Image(Point(144, 100), "blue.png")
+        blue.draw(self.win)
+        
+        
+        self.button = Quizans(self.win)
+
+        self.quiz = Question(self.win)
+        self.count = 0
+
+    def run(self):
+        len = self.quiz.quizlen()
+
+        for index in range(len):
+            self.quiz.getquiz(index)
+            topic_answer = self.quiz.getanswer()
+            self.quiz.lookquiz()
+
+            player_answer = self.button.answer()
+            self.quiz.lookright(player_answer)
+            self.quiz.lookans()
+            if player_answer == topic_answer:
+                self.count = self.count + 1
+
+            next = False
+            while(next == False):
+                next = self.button.next()
+            
+            self.quiz.undraw()
+        
+        self.quiz.result(self.count)
+            
+        while True:
+    
+            key = self.win.checkKey()
+            if key in ["q", "Q"]:
+                break
+        self.win.close()
+                    
+                                                                                                                                                                                                                                                                                                                                                                                                                                             
 
 if __name__ == "__main__":
-    ProjectileApp().run()
+    Launch().run()
